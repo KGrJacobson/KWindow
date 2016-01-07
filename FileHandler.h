023b318cdaa0a5e55b21890e36b9fcac9d8ddb73
Kyle <KGrJacobson@gmail.com>
2016-01-07 16:18:32 -0600

    Backup for UI menu revamp
    Also more kanji

--- FileHandler.h
+++ FileHandler.h
@@ -1,10 +1,17 @@
 #ifndef FILE_HANDLER
 #define FILE_HANDLER
+#include <fstream>
+#include <string>
 
 class FileHandler
 {
 public:
-	
+
+private:
+	std::string filename_;
+	std::fstream filestream_;
+	uint8_t dataflag_;
+	uint16_t nextbytesize_;
 };
 
 #endif //FILE_HANDLER
